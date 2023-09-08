4e5ef7e838bf0fedb8356495555c0712edaa0ccc
Harinder Kaur <harinder031998kaur@gmail.com>
2023-09-08 12:25:47 +1000

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,17 @@
+//Import needed files from library
+
+
+//Creating a class
+
+public class Assignment1
+{
+    public void enterAssignmnetName()
+    {
+        System.out.println("Welcome");  //Welcome statement of the program
+      /*   String assignmentName;          // Variable which will contain the assignment name
+        Scanner input = new.Scanner(System.in);
+        this.assignmentName = input.nextLine(); //recieve assignmnet name
+        input.close();*/
+    }
+}
+
